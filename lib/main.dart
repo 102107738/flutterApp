670d4101a1be6679aaff56762af6b093c43d4b16
hanad-alm-d <68382935+hanad-alm-d@users.noreply.github.com>
2023-06-25 01:40:52 +1000

    bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ void main() {
 
 //AppBar widget is used to create a navigation bar at the top of the screen. It typically contains a title, leading and trailing widgets, and optional actions.
 //body property of the Scaffold widget is used to define the main content of the screen. It represents the area below the app bar and above the bottom navigation bar (if present).
+
+//The bottomNavigationBar property in Flutter is used to define a navigation bar at the bottom of the screen, typically containing multiple items for easy navigation between different sections or pages of an app.
+//Each item is represented by a BottomNavigationBarItem containing a label and an icon, in this case, both using the Icons.home icon.
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -25,7 +28,23 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Basic App")
         ),
-        body: Text("Body")
+        body: Text("Body"),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              label: "Home",
+            icon: Icon(Icons.home)
+
+          ),
+            BottomNavigationBarItem(
+              label: "Home",
+                icon: Icon(Icons.home)
+
+            )
+
+          ],
+        ),
+
       ),
     );
   }
