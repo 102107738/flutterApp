6a8e8972db12b75af0037c5d81ce865c2e084f2f
hanad-alm-d <68382935+hanad-alm-d@users.noreply.github.com>
2023-06-25 01:07:05 +1000

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
-//Returning a MaterialApp Widget. Scaffhold is the skeleton of the app.
+//home is an argument, Scaffold is a widget.
+//Widget start with uppercase and argument start with lowercase
+//Returning a MaterialApp Widget. Scaffold is the skeleton of the app.
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+    return MaterialApp(
+
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Basic App"),
+        ),
+      ),
     );
   }
 }
