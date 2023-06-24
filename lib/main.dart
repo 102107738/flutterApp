b15c58f0a70094f668dd19a45056aabe034f80ab
hanad-alm-d <68382935+hanad-alm-d@users.noreply.github.com>
2023-06-25 01:23:51 +1000

    Body

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 
@@ -8,6 +10,9 @@ void main() {
 //home is an argument, Scaffold is a widget.
 //Widget start with uppercase and argument start with lowercase
 //Returning a MaterialApp Widget. Scaffold is the skeleton of the app.
+
+//AppBar widget is used to create a navigation bar at the top of the screen. It typically contains a title, leading and trailing widgets, and optional actions.
+//body property of the Scaffold widget is used to define the main content of the screen. It represents the area below the app bar and above the bottom navigation bar (if present).
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -15,10 +20,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Basic App"),
+          title: Text("Basic App")
         ),
+        body: Text("Body")
       ),
     );
   }
